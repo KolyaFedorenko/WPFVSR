bf35d7857334674e0b9226b305518f510dd0d12d
Kolya Fedorenko <kolyafedorenko645@gmail.com>
2021-10-28 19:07:43 +0700

    WPFVSR

---
+++ WPFVSR/AddInfoWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFVSR
+{
+    /// <summary>
+    /// Логика взаимодействия для AddInfoWindow.xaml
+    /// </summary>
+    public partial class AddInfoWindow : Window
+    {
+        MainWindow mw = (MainWindow)Application.Current.MainWindow;
+        VSREntities context;
+        
+        public AddInfoWindow(VSREntities context, Tickets newTickets)
+        {
+            InitializeComponent();
+            this.context = context;
+            this.DataContext = newTickets;
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                context.SaveChanges();
+                mw.ShowTable();
+                this.Close();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ошибка: " + exception.Message);
+            }
+        }
+    }
+}
--- WPFVSR/MainWindow.xaml.cs
+++ WPFVSR/MainWindow.xaml.cs
@@ -20,9 +20,61 @@ namespace WPFVSR
     /// </summary>
     public partial class MainWindow : Window
     {
+        VSREntities context;
+
         public MainWindow()
         {
             InitializeComponent();
+            context = new VSREntities();
+            ShowTable();
+        }
+
+        public void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var newTickets = new Tickets();
+            context.Tickets.Add(newTickets);
+            var win = new AddInfoWindow(context, newTickets);
+            win.ShowDialog();
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var row = DataGridRegistrations.SelectedItem as Tickets;
+            if (row == null)
+            {
+                MessageBox.Show("Выберите строку для удаления");
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show("Удалить эту строку данных?", "Подтверждение удаления", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    context.Tickets.Remove(row);
+                    context.SaveChanges();
+                    ShowTable();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка:" + ex);
+                }
+
+            }
+        }
+
+        public void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnEdit = sender as Button;
+            var editTickets = btnEdit.DataContext as Tickets;
+            var win = new AddInfoWindow(context, editTickets);
+            win.ShowDialog();
+        }
+
+
+        public void ShowTable()
+        {
+            DataGridRegistrations.ItemsSource = context.Tickets.ToList();
         }
     }
 }
